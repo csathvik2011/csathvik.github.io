6905b93fb86b03f2d75d48595e217a13e846c492
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-04-10 16:07:32 -0400

    Create flappy.py

---
+++ flappy.py
@@ -0,0 +1,14 @@
+import flappybird
+score = "0"
+print("Count until you die")
+dead = exit()
+if dead:
+print("game over)
+str(input("Whats your score"))
+elif:
+print("Nice score")
+break
+else:
+print("Not functioning")
+break
+
