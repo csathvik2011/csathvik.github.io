fcaf1304f4025039f366bf59d6ca96e0e81a72fe
csathvik2011 <97310465+csathvik2011@users.noreply.github.com>
2022-04-10 16:16:15 -0400

    Update flappy.py
    Just a little python for the scoreboard.

--- flappy.py
+++ flappy.py
@@ -1,14 +1,17 @@
 import flappybird
-score = "0"
+score = 0
 print("Count until you die")
 dead = exit()
 if dead:
-print("game over)
-str(input("Whats your score"))
+print("game over")
+int(input("Whats your score"))
 elif:
 print("Nice score")
 break
 else:
 print("Not functioning")
 break
-
+if score > 100:
+print("thatts it")
+elif:
+print("impressive")
